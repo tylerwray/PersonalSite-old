dd5f9b61c81ce1bee6900d833bc3280b28ff4558
Tyler Wray <wraytw@gmail.com>
2016-07-28 23:40:39 -0600

    - Split JQuery into its own file - Styled the Header and FAB to not scroll on body - Changed box shadow to overlay - Added my name at the top of the site

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,86 @@
-$(document).ready(function(){
-  $('.fab').click (function(){
-    $('.radial').toggleClass('open');
-  });
+$(document).ready(function() {
+    $('#fab').click(function() {
+        $('#fab').toggleClass('open');
+        $('#fab').toggleClass('closed');
+        $('#fab-1').toggleClass('open');
+        $('#fab-1').toggleClass('closed');
+        $('#fab-2').toggleClass('open');
+        $('#fab-2').toggleClass('closed');
+        $('#fab-3').toggleClass('open');
+        $('#fab-3').toggleClass('closed');
+        $('#lbl-1').toggleClass('open');
+        $('#lbl-1').toggleClass('closed');
+        $('#lbl-2').toggleClass('open');
+        $('#lbl-2').toggleClass('closed');
+        $('#lbl-3').toggleClass('open');
+        $('#lbl-3').toggleClass('closed');
+        $('#overlay-fab').toggleClass('closed');
+        $('#overlay-fab').toggleClass('open');
+
+        // Toggle the body scrollability
+        if ($('body').css('overflow') == 'auto')
+        {
+            $('body').css('overflow', 'hidden');
+        }
+        else
+        {
+            $('body').css('overflow', 'auto');
+        }
+
+
+    });
+
+    $('#menu-btn').click(function() {
+        $('#side-nav').toggleClass('open');
+        $('#side-nav').toggleClass('closed');
+        $('#overlay').toggleClass('open');
+        $('#overlay').toggleClass('closed');
+
+        // Toggle the body scrollability
+        if ($('body').css('overflow') == 'auto')
+        {
+            $('body').css('overflow', 'hidden');
+        }
+        else
+        {
+            $('body').css('overflow', 'auto');
+        }
+    });
+
+    $('#overlay').click(function() {
+        $('#side-nav').toggleClass('open');
+        $('#side-nav').toggleClass('closed');
+        $('#overlay').toggleClass('open');
+
+        // Toggle the body scrollability
+        if ($('body').css('overflow') == 'hidden')
+        {
+            $('body').css('overflow', 'auto');
+        }
+    });
+
+    $('#overlay-fab').click(function() {
+        $('#fab').toggleClass('open');
+        $('#fab').toggleClass('closed');
+        $('#fab-1').toggleClass('open');
+        $('#fab-1').toggleClass('closed');
+        $('#fab-2').toggleClass('open');
+        $('#fab-2').toggleClass('closed');
+        $('#fab-3').toggleClass('open');
+        $('#fab-3').toggleClass('closed');
+        $('#lbl-1').toggleClass('open');
+        $('#lbl-1').toggleClass('closed');
+        $('#lbl-2').toggleClass('open');
+        $('#lbl-2').toggleClass('closed');
+        $('#lbl-3').toggleClass('open');
+        $('#lbl-3').toggleClass('closed');
+        $('#overlay-fab').toggleClass('closed');
+        $('#overlay-fab').toggleClass('open');
+
+        // Toggle the body scrollability
+        if ($('body').css('overflow') == 'hidden')
+        {
+            $('body').css('overflow', 'auto');
+        }
+    });
 });
