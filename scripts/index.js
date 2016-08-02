3394b818ee77dcf76975fd74f160e5bdddca93d1
Tyler Wray <wraytw@gmail.com>
2016-08-01 22:29:00 -0600

    Added Shit

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,35 @@
+function closeAll() {
+    $('#fab').removeClass('open');
+    $('#fab-1').removeClass('open');
+    $('#fab-2').removeClass('open');
+    $('#fab-3').removeClass('open');
+    $('#lbl-1').removeClass('open');
+    $('#lbl-2').removeClass('open');
+    $('#lbl-3').removeClass('open');
+    $('#overlay').removeClass('open');
+    $('#overlay-fab').removeClass('open');
+
+    if ($('body').css('overflow') == 'hidden')
+    {
+        $('body').css('overflow', 'auto');
+    }
+}
+
+function writeContent(one) {
+    document.write(one);
+}
+
 $(document).ready(function() {
+
+    // Click Event Handler for the Fab
     $('#fab').click(function() {
         $('#fab').toggleClass('open');
-        $('#fab').toggleClass('closed');
         $('#fab-1').toggleClass('open');
-        $('#fab-1').toggleClass('closed');
         $('#fab-2').toggleClass('open');
-        $('#fab-2').toggleClass('closed');
         $('#fab-3').toggleClass('open');
-        $('#fab-3').toggleClass('closed');
         $('#lbl-1').toggleClass('open');
-        $('#lbl-1').toggleClass('closed');
         $('#lbl-2').toggleClass('open');
-        $('#lbl-2').toggleClass('closed');
         $('#lbl-3').toggleClass('open');
-        $('#lbl-3').toggleClass('closed');
-        $('#overlay-fab').toggleClass('closed');
         $('#overlay-fab').toggleClass('open');
 
         // Toggle the body scrollability
@@ -32,9 +47,7 @@ $(document).ready(function() {
 
     $('#menu-btn').click(function() {
         $('#side-nav').toggleClass('open');
-        $('#side-nav').toggleClass('closed');
         $('#overlay').toggleClass('open');
-        $('#overlay').toggleClass('closed');
 
         // Toggle the body scrollability
         if ($('body').css('overflow') == 'auto')
@@ -49,7 +62,6 @@ $(document).ready(function() {
 
     $('#overlay').click(function() {
         $('#side-nav').toggleClass('open');
-        $('#side-nav').toggleClass('closed');
         $('#overlay').toggleClass('open');
 
         // Toggle the body scrollability
@@ -61,21 +73,31 @@ $(document).ready(function() {
 
     $('#overlay-fab').click(function() {
         $('#fab').toggleClass('open');
-        $('#fab').toggleClass('closed');
         $('#fab-1').toggleClass('open');
-        $('#fab-1').toggleClass('closed');
         $('#fab-2').toggleClass('open');
-        $('#fab-2').toggleClass('closed');
         $('#fab-3').toggleClass('open');
-        $('#fab-3').toggleClass('closed');
         $('#lbl-1').toggleClass('open');
-        $('#lbl-1').toggleClass('closed');
         $('#lbl-2').toggleClass('open');
-        $('#lbl-2').toggleClass('closed');
         $('#lbl-3').toggleClass('open');
-        $('#lbl-3').toggleClass('closed');
-        $('#overlay-fab').toggleClass('closed');
         $('#overlay-fab').toggleClass('open');
+
+        // Toggle the body scrollability
+        if ($('body').css('overflow') == 'hidden')
+        {
+            $('body').css('overflow', 'auto');
+        }
+    });
+
+    $('.side-nav-item, .side-nav-item-lower').click(function() {
+        $('.side-nav-item, .side-nav-item-lower').removeClass('active');
+        $(this).toggleClass('active');
+        $('#side-nav').toggleClass('open');
+        closeAll();
+    });
+
+    $('.side-nav-header').click(function() {
+        $('#side-nav').toggleClass('open');
+        $('#overlay').toggleClass('open');
 
         // Toggle the body scrollability
         if ($('body').css('overflow') == 'hidden')
