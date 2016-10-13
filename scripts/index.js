b591335701910fff877780300ac12d806f0c1f47
Tyler Wray <wraytw@gmail.com>
2016-10-13 10:18:01 -0600

    Minor Changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,4 @@
-function closeAll() {
-    $('#fab').removeClass('open');
-    $('#fab-1').removeClass('open');
-    $('#fab-2').removeClass('open');
-    $('#fab-3').removeClass('open');
-    $('#lbl-1').removeClass('open');
-    $('#lbl-2').removeClass('open');
-    $('#lbl-3').removeClass('open');
-    $('#overlay').removeClass('open');
-    $('#overlay-fab').removeClass('open');
-
-    if ($('body').css('overflow') == 'hidden')
-    {
-        $('body').css('overflow', 'auto');
-    }
-}
-
-function toggleOverlay() {
-    $('#overlay').toggleClass('open');
-
-    // Toggle the body scrollability
-    if ($('body').css('overflow') == 'hidden')
-    {
-        $('body').css('overflow', 'auto');
-    }
-    else
-    {
-        $('body').css('overflow', 'hidden');
-    }
-}
-
-function writeContent(one) {
-    document.write(one);
-}
-
 $(document).ready(function() {
-
     // Click Event Handler for the Fab
     $('#fab').click(function() {
         $('#fab').toggleClass('open');
